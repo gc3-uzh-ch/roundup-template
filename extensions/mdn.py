63cdeb523b6bd69687096e4a3be1008bf121ea0a
Antonio Messina <antonio.s.messina@gmail.com>
2014-04-30 12:31:30 +0200

    Cosmetic changes

--- extensions/mdn.py
+++ extensions/mdn.py
@@ -2,7 +2,7 @@ import markdown as markdown_module
 
 def markdown(text):
   return markdown_module.markdown(text, 
-      ['codehilite', 'extra'])
+                                  ['codehilite', 'extra', 'nl2br'])
 
 def init(instance):
   instance.registerUtil('markdown', markdown)
