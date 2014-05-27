7c5720375cac8cc3ccfc8166f40ce52bc0664830
Antonio Messina <antonio.s.messina@gmail.com>
2014-05-27 17:39:59 +0200

    Uses markdown2 instead of markdown. Fixes issue67: https://www.s3it.uzh.ch/help/issue67

--- extensions/mdn.py
+++ extensions/mdn.py
@@ -1,8 +1,22 @@
-import markdown as markdown_module
+# import markdown as markdown_module
+
+# def markdown(text):
+#   return markdown_module.markdown(text, 
+#                                   ['codehilite', 'extra', 'nl2br'])
+
+import markdown2 as markdown_module
+import re
+
+
+link_patterns = [
+    (re.compile(r'\b(issue\d+)\b'), r'/help/\1'),
+    (re.compile(r'''(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))'''), r'\1'),
+  ]
 
 def markdown(text):
-  return markdown_module.markdown(text, 
-                                  ['codehilite', 'extra', 'nl2br'])
+  return markdown_module.markdown(
+      text, extras=['footnotes', 'fenced-code-blocks', 'link-patterns'],
+      link_patterns=link_patterns)
 
 def init(instance):
   instance.registerUtil('markdown', markdown)
